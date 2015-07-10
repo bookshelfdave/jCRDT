facdbe841a98d520a8969e28030b1a5d5f1ff667
Dave Parfitt <dparfitt@chef.io>
2015-07-09 20:07:52 -0400

    initial add of PNCounter

---
+++ src/main/java/org/metadave/jcrdt/PNCounter.java
@@ -0,0 +1,31 @@
+package org.metadave.jcrdt;
+
+/**
+ * Created by dparfitt on 7/9/15.
+ */
+public class PNCounter {
+    GCounter p;
+    GCounter n;
+
+    public void inc(String id) {
+        inc(id, 1);
+    }
+    public void inc(String id, long tosum) {
+        p.inc(id, tosum);
+    }
+
+    public void dec(String id) {
+        dec(id, 1);
+    }
+    public void dec(String id, long tosum) {
+        n.inc(id, tosum);
+    }
+
+    public long read() {
+        return p.read() - n.read();
+    }
+    public void join(PNCounter o) {
+        p.join(o.p);
+        n.join(o.n);
+    }
+}
--- src/test/java/org/metadave/jcrdt/GCounterTest.java
+++ src/test/java/org/metadave/jcrdt/GCounterTest.java
@@ -1,6 +1,7 @@
 package org.metadave.jcrdt;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
  * Created by dparfitt on 7/9/15.
@@ -8,27 +9,24 @@ import org.junit.Test;
 public class GCounterTest {
     @Test
     public void testGCounter() {
-        /*
-        GCounter: ( idx->5 idy->2 )
-        GCounter: ( idx->5 idy->2 )
-         */
-
         GCounter gc1 = new GCounter();
-
         GCounter gc2 = new GCounter();
+
         gc1.inc("foo");
         gc1.inc("foo");
         gc1.inc("foo");
+        gc1.inc("bar");
         gc1.inc("bar");
 
         gc2.inc("bar");
         gc2.inc("baz");
         gc2.inc("baz");
 
-        System.out.println(gc1.read());
-        System.out.println(gc2.read());
+        assertEquals(5, gc1.read());
+        assertEquals(3, gc2.read());
 
         gc1.join(gc2);
-        System.out.println(gc1.read());
+        assertEquals(7, gc1.read()); // max of 2 vs 1 in gc1 and gc2 = 2
     }
+
 }
